02818b0da9d70fbfa3b0720ecf8864e0278e1e10
Roger Andersen <hexjelly@hexjelly.com>
2017-10-05 08:57:57 +0200

    add(test): basic test structure

--- test/integration/routes.js
+++ test/integration/routes.js
@@ -1,1 +1,17 @@
+import test from 'ava';
 
+test('/: returns 200 and html', t => {
+	t.fail();
+});
+
+test('/api/: valid data returns 200 and json', t => {
+	t.fail();
+});
+
+test('/api/: invalid data returns 400 and json with error', t => {
+	t.fail();
+});
+
+test('/api/: wrong field(s) returns 400 and json with error', t => {
+	t.fail();
+});
