13ee3cf5d42eed647ae0c1b6202939758116ec6f
Roger Andersen <hexjelly@hexjelly.com>
2017-10-05 09:00:42 +0200

    update(routes): add all necessary controller logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,19 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/', upload.single('file'), (req, res) => {
-	res.status(200).json( {size: req.file.size});
+	if (req.file) {
+		res.status(200).json({size: req.file.size});
+	} else {
+		res.status(400).json({error: 'Missing file input'});
+	}
+});
+
+router.use('/api/', (err, req, res, next) => {
+	if (err) {
+		res.status(400).json({error: err.message});
+	} else {
+		next();
+	}
 });
 
 module.exports = router;
